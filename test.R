32246c87346c22682dacb278596d1f5d906cbd7a
sethackerman <55926669+sethackerman@users.noreply.github.com>
2026-03-18 09:15:34 -0700

    Log character count of GOOGLE_SHEETS_KEY
    Add logging for character count of GOOGLE_SHEETS_KEY

--- test.R
+++ test.R
@@ -6,6 +6,7 @@ list1 <- c("first","second","third")
 list2 <- c("uno","dos","tres")
 df <- data.frame(list1 = list1, list2 = list2)
 key_json <- jsonlite::fromJSON(Sys.getenv("GOOGLE_SHEETS_KEY"))
+cat("Characters read:", nchar(Sys.getenv("GOOGLE_SHEETS_KEY")), "\n")
 gs4_auth(path = key_json)
 
 sheet_id <- drive_get("feedbin")$id
