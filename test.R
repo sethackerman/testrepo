cd1d7de1e5a6ecae7585e2f865de54c25a6ff2c7
sethackerman <55926669+sethackerman@users.noreply.github.com>
2026-03-18 08:15:00 -0700

    Change Google Sheets authentication method
    Updated authentication method for Google Sheets API by writing the JSON secret to a temporary file before authentication.

--- test.R
+++ test.R
@@ -5,10 +5,11 @@ list1 <- c("first","second","third")
 list2 <- c("uno","dos","tres")
 df <- as.data.frame(list1,list2)
 
-# Authenticate using a service account key stored as a GitHub secret
-gs4_auth(path = Sys.getenv("GOOGLE_SHEETS_KEY"))
+# Write the JSON secret to a temp file, then authenticate
+key_file <- tempfile(fileext = ".json")
+writeLines(Sys.getenv("GOOGLE_SHEETS_KEY"), key_file)
+gs4_auth(path = key_file)
 
 # Upload fb_archive to the "feedbin" Google Sheet
 sheet_id <- drive_get("feedbin")$id
-
 write_sheet(df, ss = sheet_id, sheet = "Sheet1")
