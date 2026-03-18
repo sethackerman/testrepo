650da8286439e45f6b43acb11572d27b417f1801
sethackerman <55926669+sethackerman@users.noreply.github.com>
2026-03-18 09:04:43 -0700

    Change Google Sheets authentication to use JSON
    Updated authentication method for Google Sheets API.

--- test.R
+++ test.R
@@ -1,14 +1,14 @@
 library(googlesheets4)
 library(googledrive)
+library(jsonlite)
 
 list1 <- c("first","second","third")
 list2 <- c("uno","dos","tres")
 df <- as.data.frame(list1,list2)
 
-# Authenticate using a service account key stored as a GitHub secret
-gs4_auth(path = Sys.getenv("GOOGLE_SHEETS_KEY"))
+key_json <- jsonlite::fromJSON(Sys.getenv("GOOGLE_SHEETS_KEY"))
+gs4_auth(path = key_json)
 
-# Upload fb_archive to the "feedbin" Google Sheet
 sheet_id <- drive_get("feedbin")$id
 
 write_sheet(df, ss = sheet_id, sheet = "Sheet1")
