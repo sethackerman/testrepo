cfca8561c7a0b9411e0e5883b0709ca7550433c3
sethackerman <55926669+sethackerman@users.noreply.github.com>
2026-03-18 05:47:59 -0700

    Add Google Drive authentication and CSV upload
    Authenticate Google Drive and upload a CSV file.

--- test.R
+++ test.R
@@ -1,3 +1,17 @@
+library(googlesheets4)
+library(googledrive)
+
 list1 <- c("first","second","third")
 list2 <- c("uno","dos","tres")
 df <- as.data.frame(list1,list2)
+
+secret_json <- Sys.getenv("GDRIVE_SERVICE_ACCOUNT")
+tkn <- tempfile(fileext = ".json")
+writeLines(secret_json, tkn)
+drive_auth(path = tkn)
+gs4_auth(token = drive_token())
+message("Authenticated as: ", drive_user()$emailAddress)
+
+temp_file <- tempfile(fileext = ".csv")
+write.csv(df, temp_file, row.names = FALSE)
+drive_put(media = temp_file, path = as_id("1lSybfpacWavRrV3runu7vkeHXXXFTSxq"), name = "df.csv")
