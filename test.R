85086397eec99a1209dff6c2f700c23168b17fc3
sethackerman <55926669+sethackerman@users.noreply.github.com>
2026-03-18 08:08:55 -0700

    Refactor Google Sheets authentication and upload
    Updated authentication method for Google Sheets and modified data upload process.

--- test.R
+++ test.R
@@ -5,13 +5,10 @@ list1 <- c("first","second","third")
 list2 <- c("uno","dos","tres")
 df <- as.data.frame(list1,list2)
 
-secret_json <- Sys.getenv("GDRIVE_SERVICE_ACCOUNT")
-tkn <- tempfile(fileext = ".json")
-writeLines(secret_json, tkn)
-drive_auth(path = tkn)
-gs4_auth(token = drive_token())
-message("Authenticated as: ", drive_user()$emailAddress)
+# Authenticate using a service account key stored as a GitHub secret
+gs4_auth(path = Sys.getenv("GOOGLE_SHEETS_KEY"))
 
-temp_file <- tempfile(fileext = ".csv")
-write.csv(df, temp_file, row.names = FALSE)
-drive_put(media = temp_file, path = as_id("1lSybfpacWavRrV3runu7vkeHXXXFTSxq"), name = "df.csv")
+# Upload fb_archive to the "feedbin" Google Sheet
+sheet_id <- drive_get("feedbin")$id
+
+write_sheet(df, ss = sheet_id, sheet = "Sheet1")
